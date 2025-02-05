36d0467110ec91c7c9fb7f803bb373e75b1bbf19
William Roberts <werobert@yahoo.com>
2025-02-05 18:54:52 +0000

    college-model upd.

--- backend/college/models.py
+++ backend/college/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Types(models.TextChoices):
+    public = "Public"
+    private = "Private"
+
+class Division(models.TextChoices):
+    D1 = "D1"
+    D2 = "D2"
+    D3 = "D3"
+
+class College(models.Model):
+    name = models.CharField(MaxLength=100, null=True)
+    address = models.CharField(max_length=100, null=True)
+    tuition = models.IntegerField(max_length=20, null=True)
+    type = models.CharField(choices=Types.choices, default=Types.public)
+    established = models.IntegerField(max_length=4, null=True)
+    endowment = models.IntegerField(max_length=20, null=True)
+    academicStaff = models.IntegerField(max_length=10, null=True)
+    undergrad = models.IntegerField(max_length=10, null=True)
+    postgrad = models.IntegerField(max_length=10, null=True)
+    campusSize = models.IntegerField(max_length=10, null=True)
+    division = models.CharField(choices=Division.choices, default=Division.D1)
