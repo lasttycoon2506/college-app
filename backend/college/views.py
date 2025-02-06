bccbca6abaa9e0c260b920174e6d9f72e9b1a0d4
William Roberts <werobert@yahoo.com>
2025-02-06 21:15:04 +0000

    edit-clg view

--- backend/college/views.py
+++ backend/college/views.py
@@ -30,3 +30,25 @@ def createCollege(request):
 
     return Response(newCollegeSerialized.data)
 
+@api_view(['PUT'])
+def editCollege(request, id):
+    collegeToEdit = get_object_or_404(College, id=id)
+
+    collegeToEdit.academicStaff = request.data['academicStaff']
+    collegeToEdit.address = request.data['address']
+    collegeToEdit.campusSize = request.data['campuseSize']
+    collegeToEdit.division = request.data['division']
+    collegeToEdit.endowment = request.data['endowment']
+    collegeToEdit.established = request.data['established']
+    collegeToEdit.undergrad = request.data['undergrad']
+    collegeToEdit.type = request.data['type']
+    collegeToEdit.tuition = request.data['tuition']
+    collegeToEdit.postgrad = request.data['postgrad']
+    collegeToEdit.name = request.data['name']
+
+    collegeToEdit.save()
+
+    collegeToEditSerialized = CollegeSerializer(collegeToEdit, many=False)
+
+    return Response(collegeToEditSerialized)
+
