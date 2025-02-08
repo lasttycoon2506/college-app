6bebb65c0d6319b6f14b43402d8f1120f96e5bdd
William Roberts <werobert@yahoo.com>
2025-02-08 18:51:17 +0000

    max-tuition filter

--- backend/college/filters.py
+++ backend/college/filters.py
@@ -2,6 +2,9 @@ from django_filters import rest_framework as filters
 from .models import College
 
 class CollegesFilter(filters.FilterSet):
+
+    max_tuition = filters.NumberFilter(field_name="tuition" or 1000000, lookup_expr='lte')
+
     class Meta:
         model = College
-        fields = ('type', 'division')
+        fields = ('type', 'division', 'max_tuition')
