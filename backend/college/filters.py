973be5e80c84711e5d34bbcfcb22ffdf2ea41856
William Roberts <werobert@yahoo.com>
2025-02-08 22:09:32 +0000

    undergrad filters

--- backend/college/filters.py
+++ backend/college/filters.py
@@ -4,7 +4,9 @@ from .models import College
 class CollegesFilter(filters.FilterSet):
 
     max_tuition = filters.NumberFilter(field_name="tuition" or 1000000, lookup_expr='lte')
+    min_undergrad = filters.NumberFilter(field_name="undergrad" or 1000000, lookup_expr='gte')
+    max_undergrad = filters.NumberFilter(field_name="undergrad" or 1000000, lookup_expr='lte')
 
     class Meta:
         model = College
-        fields = ('type', 'division', 'max_tuition')
+        fields = ('type', 'division', 'max_tuition', 'min_undergrad', 'max_undergrad')
