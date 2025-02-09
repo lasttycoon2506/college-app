9b147ac15a69dfa36c62f5fb5630f412f67c6700
William Roberts <werobert@yahoo.com>
2025-02-09 20:43:42 +0000

    get cur user

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,12 +1,13 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import SignUpSerializer
+from .serializers import SignUpSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.hashers import make_password
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def register(request):
     data = request.data
     user = SignUpSerializer(data=data)
@@ -24,3 +25,11 @@ def register(request):
             return Response({"error": "email already registered"}, status=status.HTTP_200_OK)
     else:
         return Response(user.errors)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def getCurrUser(request):
+    currUser = UserSerializer(request.user)
+
+    return Response(currUser.data)
