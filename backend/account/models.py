4037e2064bd95173b82156378c41fa5189cb74c6
William Roberts <werobert@yahoo.com>
2025-02-10 21:58:21 +0000

    userprofile caps

--- backend/account/models.py
+++ backend/account/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name="userProfile", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
     essay = models.FileField(null=True)
     sat = models.IntegerField(null=True)
     gpa = models.IntegerField(null=True)
--- backend/account/views.py
+++ backend/account/views.py
@@ -64,7 +64,7 @@ def uploadEssay(request):
     if file is None:
         return Response({"error": "missing essay"}, status=status.HTTP_400_BAD_REQUEST)
 
-    user.userProfile.essay = file
-    user.userProfile.save()
+    user.userprofile.essay = file
+    user.userprofile.save()
 
     return Response(status=status.HTTP_200_OK)
