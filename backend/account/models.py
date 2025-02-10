e155a2ea448cd0c5f0ac02f241a616fc5e78c295
William Roberts <werobert@yahoo.com>
2025-02-10 21:52:36 +0000

    profile signal

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
@@ -7,3 +9,13 @@ class UserProfile(models.Model):
     essay = models.FileField(null=True)
     sat = models.IntegerField(null=True)
     gpa = models.IntegerField(null=True)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+
+    user = instance
+
+    if created:
+        profile = UserProfile(user=user)
+        profile.save()
