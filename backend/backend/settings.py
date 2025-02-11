d93e092cb25ac2cf763d175f8996ed15b770746d
William Roberts <werobert@yahoo.com>
2025-02-11 21:09:13 +0000

    aws-settings typo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
-DEFAULT_FILE_STORE = "storages.backends.s3boto3.s3Boto3Storage"
+DEFAULT_FILE_STORE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 # Static files (CSS, JavaScript, Images)
