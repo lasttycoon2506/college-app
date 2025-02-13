89d11193edcc833919db869629d330054e195d7e
William Roberts <werobert@yahoo.com>
2025-02-13 18:32:08 +0000

    account-serializer gpa req.fields

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -18,7 +18,7 @@ class SignUpSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     essay = serializers.CharField(source="userprofile.essay")
     sat = serializers.IntegerField(source="userprofile.sat")
-    gpa = serializers.DecimalField(source="userprofile.gpa")
+    gpa = serializers.DecimalField(source="userprofile.gpa", max_digits=4, decimal_places=2)
 
     class Meta:
         model = User
