75f193b55549f8a8fd89f27b56d97749f12c6843
William Roberts <werobert@yahoo.com>
2025-02-13 20:45:06 +0000

    cur-user apps route

--- backend/college/urls.py
+++ backend/college/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('colleges/add', views.addCollege, name='add_college'),
     path('colleges/edit/<str:id>/', views.editCollege, name='edit_college'),
     path('colleges/delete/<str:id>/', views.deleteCollege, name='delete_college'),
-    path('colleges/apply/<str:id>/', views.applyCollege, name='apply_college')
+    path('colleges/apply/<str:id>/', views.applyCollege, name='apply_college'),
+    path('currentUser/applications', views.getCurrUserApplications, name='current_user_applications')
 ]
