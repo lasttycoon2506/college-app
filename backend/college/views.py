2976769f2136c298a36a47334517ac15f542435d
William Roberts <werobert@yahoo.com>
2025-02-13 19:59:55 +0000

    college app post

--- backend/college/views.py
+++ backend/college/views.py
@@ -1,12 +1,13 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import College
+from .models import College, Applicants
 from .serializers import CollegeSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from .filters import CollegesFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
 
 
 
@@ -44,6 +45,36 @@ def addCollege(request):
     College.objects.create(**data)
 
     return Response(status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def apply(request, id):
+    applicant = request.user
+    college = get_object_or_404(College, id)
+
+    hasApplied = college.applicants_set.filter(user=applicant).exists()
+
+    if hasApplied:
+        return Response({"error": "already applied"}, status=status.HTTP_400_BAD_REQUEST)
+    if not applicant.userprofile.essay:
+        return Response({"error": "missing essay"}, status=status.HTTP_400_BAD_REQUEST)
+    if not applicant.userprofile.gpa:
+        return Response({"error": "missing gpa"}, status=status.HTTP_400_BAD_REQUEST)
+    if not applicant.userprofile.sat:
+        return Response({"error": "missing sat"}, status=status.HTTP_400_BAD_REQUEST)
+    if timezone.now() > college.applicationDeadline:
+        return Response({"error": "deadline passed!"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    collegeApplied = Applicants.objects.create(
+        college = college,
+        applicant = applicant,
+        essay = applicant.userprofile.essay,
+        sat = applicant.userprofile.sat,
+        gpa = applicant.userprofile.gpa
+    )
+    
+    return Response({'applied': True, 'applicationId': collegeApplied.id}, status=status.HTTP_200_OK)
 
 
 @api_view(['PUT'])
