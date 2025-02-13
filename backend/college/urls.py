cba6ea4401eb19a95844529d10d424ae55b079b5
William Roberts <werobert@yahoo.com>
2025-02-13 21:07:33 +0000

    hasApplied route

--- backend/college/urls.py
+++ backend/college/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('colleges/edit/<str:id>/', views.editCollege, name='edit_college'),
     path('colleges/delete/<str:id>/', views.deleteCollege, name='delete_college'),
     path('colleges/apply/<str:id>/', views.applyCollege, name='apply_college'),
-    path('currentUser/applications/', views.getCurrUserApplications, name='current_user_applications')
+    path('currentUser/applications/', views.getCurrUserApplications, name='current_user_applications'),
+    path('currentUser/applied/<str:id>/', views.hasApplied, name='current_user_applied')
 ]
