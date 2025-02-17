317c4ed51ce06120084300a2a52ea35edcf55f32
William Roberts <werobert@yahoo.com>
2025-02-17 15:46:35 -0500

    navbar styling

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,32 +1,19 @@
+import Image from "next/image";
+
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          className="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
-        <span className="font-semibold text-xl tracking-tight">
-          Tailwind CSS
-        </span>
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-3">
+      <div className="flex items-center flex-shrink-0 text-white mr-12">
+        <Image
+          className="dark:invert mr-6"
+          src="/college.svg"
+          alt="clg icon"
+          width={45}
+          height={45}
+        />
+        <span className="font-semibold text-xl tracking-tight">EZ Apply</span>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
+
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
           <a
@@ -48,13 +35,26 @@ export default function Navbar() {
             Blog
           </a>
         </div>
-        <div>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        <div className="flex flex-shrink-0 text-white">
+          <form className="bg-white shadow-md rounded" />
+          <input
+            className="shadow rounded w-full py-3 px-3 text-gray-700 leading-tight focus:shadow-outline mr-2"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            className="shadow rounded w-full py-3 px-3 text-gray-700 leading-tight focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
+            type="button"
           >
-            Download
-          </a>
+            Login
+          </button>
         </div>
       </div>
     </nav>
