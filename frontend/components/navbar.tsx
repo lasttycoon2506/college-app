ba520ad799fe0f2b7f37fee928a2d364256c2c40
William Roberts <werobert@yahoo.com>
2025-02-17 13:45:41 -0500

    removed bootstrap

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,19 +1,38 @@
 import Image from 'next/image'
+import '../app/globals.css'
 
 export default function NavBar() {
     return (
-        <nav className="navbar bg-body-tertiary">
+        <nav
+            className="navbar bg-dark border-bottom border-body"
+            data-bs-theme="light"
+        >
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand navbar-light navbar-text" href="#">
                     <Image
                         src="/college-svgrepo-com.svg"
                         alt="Logo"
                         width="30"
                         height="24"
-                        className="d-inline-block align-text-top"
+                        className="d-inline-block align-text-top me-3"
                     />
-                    Bootstrap
+                    College EZ
                 </a>
+                <form className="d-flex" role="search">
+                    <input
+                        className="form-control me-2"
+                        type="login"
+                        placeholder="Username"
+                    />
+                    <input
+                        className="form-control me-2"
+                        type="login"
+                        placeholder="Password"
+                    />
+                    <button className="btn btn-outline-success" type="submit">
+                        LOGIN
+                    </button>
+                </form>
             </div>
         </nav>
     )
