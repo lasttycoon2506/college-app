835293da19c990fd52625dc44cc858009e1d00ff
William Roberts <werobert@yahoo.com>
2025-02-18 16:24:43 -0500

    college-card style

--- frontend/components/college.tsx
+++ frontend/components/college.tsx
@@ -12,32 +12,43 @@ export default function College({ college }: { college: College }) {
   }, []);
 
   return (
-    <div className="max-w-sm w-full lg:max-w-full lg:flex">
-      <div
-        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-        style={{
-          backgroundImage: `url('/campus${randomInt}.jpg')`,
-        }}
-      ></div>
-      <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-        <div className="mb-8">
-          <div className="text-gray-900 font-bold text-xl mb-2">
-            {college.name}
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <Image
+          className="dark:invert mr-6"
+          src={`/campus${randomInt}.jpg`}
+          alt="clg campus"
+          width={200}
+          height={200}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{college.name}</h2>
+        <p>{college.address}</p>
+        <p>{college.tuition}</p>
+        <p>{college.applicationDeadline.toString()}</p>
+        <div className="grid grid-cols-10 gap-1">
+          <div className="...">
+            <Image
+              className="dark:invert mt-4 "
+              src="/info.svg"
+              alt="info icon"
+              width={20}
+              height={20}
+            />
           </div>
-          <p className="text-gray-700 text-base">{college.address}</p>
+          <div className="...">{college.established}</div>
+          <div className="...">{college.type}</div>
+          <div className="...">{college.academicStaff}</div>
+          <div className="...">{college.undergrad}</div>
+          <div className="...">{college.postgrad}</div>
+          <div className="...">{college.endowment}</div>
+          <div className="...">{college.division}</div>
+          <div className="...">{college.campusSize}</div>
         </div>
-        <div className="flex items-center">
-          <Image
-            className="w-10 h-10 rounded-full mr-4"
-            src="/college.svg"
-            alt="Avatar of Jonathan Reinink"
-            width={10}
-            height={10}
-          />
-          <div className="text-sm">
-            <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-            <p className="text-gray-600">Aug 18</p>
-          </div>
+
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
         </div>
       </div>
     </div>
