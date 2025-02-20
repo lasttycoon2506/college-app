3ba68fdbaf181205f962a12e4641e6636b7025b6
William Roberts <werobert@yahoo.com>
2025-02-20 14:06:16 -0500

    extra filters

--- frontend/components/ui/filters.tsx
+++ frontend/components/ui/filters.tsx
@@ -5,9 +5,30 @@ export default function filters() {
         <span className="label-text">Remember me</span>
         <input
           type="checkbox"
-          defaultChecked
-          className="checkbox checkbox-primary"
+          className="checkbox checkbox-primary checkbox-sm"
         />
+      </label>
+      <label className="cursor-pointer label">
+        <span className="label-text">Remember me</span>
+        <input
+          type="checkbox"
+          className="checkbox checkbox-secondary checkbox-md"
+        />
+      </label>
+      <label className="cursor-pointer label">
+        <span className="label-text">Remember me</span>
+        <input
+          type="checkbox"
+          className="checkbox checkbox-accent checkbox-lg"
+        />
+      </label>
+      <label className="cursor-pointer label">
+        <span className="label-text">Remember me</span>
+        <input type="checkbox" className="checkbox checkbox-warning" />
+      </label>
+      <label className="cursor-pointer label">
+        <span className="label-text">Remember me</span>
+        <input type="checkbox" className="checkbox checkbox-info" />
       </label>
     </div>
   );
