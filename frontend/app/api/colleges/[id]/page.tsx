5c1f52826b224de861c5e824a278ef13ae6effc7
William Roberts <werobert@yahoo.com>
2025-02-20 12:11:24 -0500

    app-deadline styling

--- frontend/app/api/colleges/[id]/page.tsx
+++ frontend/app/api/colleges/[id]/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 async function getCollegeDetails(id: number) {
   const res = await fetch(`http://localhost:8000/api/colleges/${id}`);
   return res.json();
@@ -15,6 +13,7 @@ export default async function CollegeDetails({
   const { id } = await params;
   const { picId } = await searchParams;
   const college = await getCollegeDetails(id);
+  const currentDate = new Date().toISOString().slice(0, 10);
 
   return (
     <div className="grid grid-flow-col grid-rows-3 gap-4">
@@ -34,9 +33,42 @@ export default async function CollegeDetails({
         <p className="py-2">{college.address}</p>
       </div>
       <div className="row-span-3 col-span-1 ... flex justify-center items-center">
-        <button className="btn bg-cyan-500 shadow-xl shadow-cyan-500/50 border-none">
-          Apply
-        </button>
+        <div className="grid grid-flow-col grid-rows-2 gap-4">
+          <div>
+            {currentDate > college.applicationDeadline ? (
+              <div>
+                <div className="card bg-error text-neutral-content w-96">
+                  <div className="card-body items-center text-center">
+                    <h2 className="card-title">App. Deadline</h2>
+                    <p>We are using cookies for no reason.</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Accept</button>
+                      <button className="btn btn-ghost">Deny</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <div className="card bg-success text-neutral-content w-96">
+                  <div className="card-body items-center text-center">
+                    <h2 className="card-title">App. Deadline</h2>
+                    <p>We are using cookies for no reason.</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Accept</button>
+                      <button className="btn btn-ghost">Deny</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <div>
+            <button className="btn bg-cyan-500 shadow-xl shadow-cyan-500/50 border-none">
+              Apply
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
