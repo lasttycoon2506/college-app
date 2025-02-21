dcd4b25af4edb10ab1f234a1760f086151a6736b
William Roberts <werobert@yahoo.com>
2025-02-21 17:40:30 -0500

    filters onclicks

--- frontend/components/ui/Filters.tsx
+++ frontend/components/ui/Filters.tsx
@@ -1,4 +1,9 @@
 export default function Filters() {
+  function handleClick(event: React.MouseEvent) {}
+  function checkHandler(checkBoxName: string, checkBoxValue: string): boolean {
+    return true;
+  }
+
   return (
     <div className="form-control pt-5 ps-3">
       <div className="border-b border-blue-900 mr-3">
@@ -12,6 +17,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-primary checkbox-xs"
+            id="box1"
+            name="tuition"
+            value="0-10000"
+            defaultChecked={checkHandler("tuition", "0-10000")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">$0 - $10,000 </span>
         </label>
@@ -19,6 +29,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-secondary checkbox-xs"
+            id="box2"
+            name="tuition"
+            value="10000-20000"
+            defaultChecked={checkHandler("tuition", "10000-20000")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">$10,000 - $20,000 </span>
         </label>
@@ -26,6 +41,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-accent checkbox-xs"
+            id="box3"
+            name="tuition"
+            value="20000+"
+            defaultChecked={checkHandler("tuition", "20000+")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">$20,000+ </span>
         </label>
@@ -38,6 +58,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-warning checkbox-xs"
+            id="box4"
+            name="type"
+            value="public"
+            defaultChecked={checkHandler("type", "public")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">Public </span>
         </label>
@@ -45,6 +70,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-info checkbox-xs"
+            id="box5"
+            name="type"
+            value="private"
+            defaultChecked={checkHandler("type", "private")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">Private </span>
         </label>
@@ -57,6 +87,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-error checkbox-xs"
+            id="box6"
+            name="undergrad"
+            value="0-10000"
+            defaultChecked={checkHandler("undergrad", "0-10000")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">0 - 10,000 </span>
         </label>
@@ -64,6 +99,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-primary checkbox-xs"
+            id="box7"
+            name="undergrad"
+            value="10000-25000"
+            defaultChecked={checkHandler("undergrad", "10000-25000")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">10,000 - 25,000 </span>
         </label>
@@ -71,6 +111,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-secondary checkbox-xs"
+            id="box8"
+            name="undergrad"
+            value="25000+"
+            defaultChecked={checkHandler("undergrad", "25000+")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">25,000+ </span>
         </label>
@@ -83,6 +128,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-accent checkbox-xs"
+            id="box9"
+            name="deadline"
+            value="open"
+            defaultChecked={checkHandler("deadline", "open")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">Open </span>
         </label>
@@ -90,6 +140,11 @@ export default function Filters() {
           <input
             type="checkbox"
             className="checkbox checkbox-error checkbox-xs"
+            id="box10"
+            name="deadline"
+            value="closed"
+            defaultChecked={checkHandler("deadline", "closed")}
+            onClick={(e) => handleClick(e)}
           />
           <span className="label-text ps-1">Closed </span>
         </label>
