98654d34a0a67388088107a5bfd7d7db9423ff41
William Roberts <werobert@yahoo.com>
2025-02-27 16:58:44 +0000

    get-clg path

--- backend/college/urls.py
+++ backend/college/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('colleges/', views.getAllColleges, name='colleges'),
-    path('colleges/<str:id>/', views.getCollege, name='college'),
+    path('college/<str:id>/', views.getCollege, name='college'),
     path('colleges/add', views.addCollege, name='add_college'),
     path('colleges/edit/<str:id>/', views.editCollege, name='edit_college'),
     path('colleges/delete/<str:id>/', views.deleteCollege, name='delete_college'),
