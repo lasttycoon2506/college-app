eb06c3c0e3cbfa29c06eeee9610d2a4dfeb95b3d
William Roberts <werobert@yahoo.com>
2025-02-27 18:01:06 -0500

    tuition filter

--- frontend/app/api/colleges/page.tsx
+++ frontend/app/api/colleges/page.tsx
@@ -5,12 +5,14 @@ import { PaginatedColleges } from "@/models/paginatedColleges";
 
 async function getPaginatedColleges(
   pg: number,
+  min_tuition: string,
+  max_tuition: string,
   collegeType: string,
   min_undergrad: string,
   max_undergrad: string
 ): Promise<PaginatedColleges> {
   const res = await fetch(
-    `http://localhost:8000/api/colleges/?page=${pg}&type=${collegeType}&min_undergrad=${min_undergrad}&max_undergrad=${max_undergrad}`
+    `http://localhost:8000/api/colleges/?page=${pg}&min_tuition=${min_tuition}&max_tuition${max_tuition}&type=${collegeType}&min_undergrad=${min_undergrad}&max_undergrad=${max_undergrad}`
   );
   return res.json();
 }
@@ -18,17 +20,32 @@ async function getPaginatedColleges(
 export default async function GetCollegesForPg({
   searchParams,
 }: {
-  searchParams: { page: string; collegeType: string; undergrad: string };
+  searchParams: {
+    page: string;
+    tuition: string;
+    collegeType: string;
+    undergrad: string;
+  };
 }): Promise<React.ReactNode> {
-  const { page = "1", collegeType, undergrad } = await searchParams;
+  const { page = "1", tuition, collegeType, undergrad } = await searchParams;
   const pgSize: number = 5;
+  let min_tuition = "";
+  let max_tuition = "";
   let min_undergrad = "";
   let max_undergrad = "";
+  if (tuition) {
+    [min_tuition, max_tuition] = tuition
+      .replaceAll(",", "")
+      .replaceAll("$", "")
+      .split("-");
+  }
   if (undergrad) {
     [min_undergrad, max_undergrad] = undergrad.replaceAll(",", "").split("-");
   }
   const paginatedColleges: PaginatedColleges = await getPaginatedColleges(
     Number(page),
+    min_tuition,
+    max_tuition,
     collegeType,
     min_undergrad,
     max_undergrad
