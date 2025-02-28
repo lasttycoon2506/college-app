a48d607a574920e1517c7b99111aead6b74a4dbb
William Roberts <werobert@yahoo.com>
2025-02-28 12:43:36 -0500

    state search

--- frontend/components/ui/Search.tsx
+++ frontend/components/ui/Search.tsx
@@ -1,4 +1,11 @@
+"use client";
+import { useState } from "react";
+
 export default function Search() {
+  const [keyword, setKeyword] = useState("");
+
+  function handleInputChange() {}
+
   return (
     <div className="form-control py-5 px-2">
       <div className="border-b border-blue-900">
@@ -8,6 +15,7 @@ export default function Search() {
         type="text"
         placeholder="Keyword"
         className="input input-bordered w-20 md:w-auto my-3 mx-5"
+        onChange={(e) => handleInputChange()}
       />
       <input
         type="text"
