2b46684b8e1279746b9b17b27b2f00a513e9c6b3
William Roberts <werobert@yahoo.com>
2025-03-07 12:11:04 -0500

    clg-details pgs

--- frontend/app/api/college/[id]/page.tsx
+++ frontend/app/api/college/[id]/page.tsx
@@ -30,113 +30,103 @@ export default async function GET({
   const currentDate = new Date().toISOString().slice(0, 10);
 
   return (
-    // <div className="grid grid-cols-3 gap-5">
-    //   <div>
-    //     <img
-    //       src={`/campus${picId}.jpg`}
-    //       className="max-w-sm rounded-lg shadow-2xl mt-7 ms-7"
-    //     />
-    //   </div>
-    //   <div className="bg-neutral-content rounded-lg mt-7">
-    //     <div className="bg-primary-content rounded-lg py-5">
-    //       <h1 className="text-5xl font-bold text-center">{college!.name}</h1>
-    //     </div>
-    //     <p className="pt-2 text-center">{college!.address}</p>
-    //     <p className="py-2 text-center">
-    //       <strong>$</strong>
-    //       {college!.tuition} / semester
-    //     </p>
-    //     <p className="py-2 text-center">{college!.type}</p>
-    //     <p className="py-2 text-center">
-    //       <strong>Est:</strong> {college!.established}
-    //     </p>
-    //     <p className="py-2 text-center">
-    //       <strong>Endowment: $</strong>
-    //       {college!.endowment}
-    //     </p>
-    //     <p className="py-2 text-center">
-    //       <strong>Academic Staff: </strong> {college!.academicStaff}
-    //     </p>
-    //     <p className="py-2 text-center">
-    //       <strong>Undergrad: </strong>
-    //       {college!.undergrad}
-    //     </p>
-    //     <p className="py-2 text-center">
-    //       <strong>Postgrad: </strong>
-    //       {college!.postgrad}
-    //     </p>
-    //     <p className="py-2 text-center">
-    //       <strong>Size: </strong>
-    //       {college!.campusSize} acres
-    //     </p>
-    //     <p className="py-2 text-center">
-    //       <strong>Division:</strong> {college!.division}
-    //     </p>
-    //   </div>
-    //   <div>
-    //     {currentDate >
-    //     new Date(college!.applicationDeadline).toISOString().slice(0, 10) ? (
-    //       <div className="grid grid-rows-2 gap-4 mt-20">
-    //         <div>
-    //           <div className="card bg-error text-neutral-content w-96">
-    //             <div className="card-body items-center text-center">
-    //               <h2 className="card-title">App. Deadline</h2>
-    //               <p>
-    //                 {new Date(college!.applicationDeadline)
-    //                   .toISOString()
-    //                   .slice(0, 10)}
-    //               </p>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     ) : (
-    //       <div className="grid grid-rows-2 gap-4 mt-20">
-    //         <div>
-    //           <div className="card bg-info text-neutral-content w-96">
-    //             <div className="card-body items-center text-center">
-    //               <h2 className="card-title">App. Deadline</h2>
-    //               <p>
-    //                 {new Date(college!.applicationDeadline)
-    //                   .toISOString()
-    //                   .slice(0, 10)}
-    //               </p>
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <div>
-    //           <div className="flex justify-center items-center m-10">
-    //             <button className="btn btn-lg btn-wide bg-success shadow-xl shadow-green-500/50 border-none text-2xl">
-    //               Apply
-    //             </button>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     )}
-    //   </div>
-    // </div>
-    <section className="bg-blue-50">
+    <div className="bg-blue-50">
       <div className="container m-auto pt-5 pb-24">
         <div className="card card-side bg-base-100 shadow-lg">
-          <figure
-            style={{ position: "relative", width: "400px", height: "400px" }}
-          >
+          <div className="flex justify-center items-center">
             <img
-              height={10}
-              width={10}
               src={`/campus${picId}.jpg`}
-              className="rounded-lg shadow-2xl mt-7 ms-7 "
+              className="max-w-sm rounded-lg shadow-2xl mt-7 mx-7"
+              style={{ height: 300 }}
             />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{college.name}</h2>
-            <p>Click the button to watch on Jetflix app.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Watch</button>
+          </div>
+          <div className="rounded-lg mt-7">
+            <div className="rounded-lg py-5">
+              <h1 className="text-5xl font-bold text-center">
+                {college!.name}
+              </h1>
             </div>
+            <p className="pt-2 text-center">{college!.address}</p>
+            <p className="py-2 text-center">
+              <strong>$</strong>
+              {college!.tuition} / semester
+            </p>
+            <p className="py-2 text-center">{college!.type}</p>
+            <p className="py-2 text-center">
+              <strong>Est:</strong> {college!.established}
+            </p>
+            <p className="py-2 text-center">
+              <strong>Endowment: $</strong>
+              {college!.endowment}
+            </p>
+            <p className="py-2 text-center">
+              <strong>Academic Staff: </strong> {college!.academicStaff}
+            </p>
+            <p className="py-2 text-center">
+              <strong>Undergrad: </strong>
+              {college!.undergrad}
+            </p>
+            <p className="py-2 text-center">
+              <strong>Postgrad: </strong>
+              {college!.postgrad}
+            </p>
+            <p className="py-2 text-center">
+              <strong>Size: </strong>
+              {college!.campusSize} acres
+            </p>
+            <p className="py-2 text-center">
+              <strong>Division:</strong> {college!.division}
+            </p>
+          </div>
+          <div>
+            {currentDate >
+            new Date(college!.applicationDeadline)
+              .toISOString()
+              .slice(0, 10) ? (
+              <div className="grid grid-rows-2 gap-4 mt-20">
+                <div className="flex justify-center items-center">
+                  <div className="card bg-error text-neutral-content w-60">
+                    <div className="card-body text-center">
+                      <h2 className="card-title justify-center">
+                        App. Deadline
+                      </h2>
+                      <p>
+                        {new Date(college!.applicationDeadline)
+                          .toISOString()
+                          .slice(0, 10)}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="grid grid-rows-2 gap-4 mt-20">
+                <div className="flex justify-center items-center">
+                  <div className="card bg-info text-neutral-content w-60">
+                    <div className="card-body text-center">
+                      <h2 className="card-title justify-center text-red-500">
+                        App. Deadline
+                      </h2>
+                      <p>
+                        {new Date(college!.applicationDeadline)
+                          .toISOString()
+                          .slice(0, 10)}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex justify-center items-center m-10">
+                    <button className="btn btn-lg btn-wide bg-success shadow-xl shadow-green-500/50 border-none text-2xl">
+                      Apply
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
