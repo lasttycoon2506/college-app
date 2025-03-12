3835c1a237d08d9ce42c9375222a1bb7b368c47a
William Roberts <werobert@yahoo.com>
2025-03-12 13:05:41 -0400

    put user valid api err handling

--- frontend/app/api/editUser/route.ts
+++ frontend/app/api/editUser/route.ts
@@ -4,42 +4,47 @@ import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adap
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-async function editUser(token: string, userData: EditUser): Promise<any> {
-  const response = await fetch("http://localhost:8000/api/currentUser/edit/", {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      first_name: userData.firstName,
-      last_name: userData.lastName,
-      email: userData.email,
-      username: userData.email,
-      password: userData.password,
-      sat: userData.sat,
-      gpa: userData.gpa,
-      essay: userData.essay,
-    }),
-  });
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+async function editUser(token: string, userData: EditUser) {
+  try {
+    const response = await fetch(
+      "http://localhost:8000/api/currentUser/edit/",
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: userData.firstName,
+          last_name: userData.lastName,
+          email: userData.email,
+          username: userData.email,
+          password: userData.password,
+          sat: userData.sat,
+          gpa: userData.gpa,
+          essay: userData.essay,
+        }),
+      }
+    );
 
-  return response.json();
+    if (!response.ok) {
+      const error = await response.json();
+      return error;
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function PUT(req: NextRequest): Promise<NextResponse> {
   const userData: EditUser = await req.json();
   const cookieStore: ReadonlyRequestCookies = await cookies();
   const token: RequestCookie | undefined = cookieStore.get("authToken");
-  try {
-    const x = await editUser(token!.value, userData);
-  } catch (error: any) {
-    return NextResponse.json(
-      { message: error.message },
-      { status: error.status }
-    );
+  const res = await editUser(token!.value, userData);
+  if (res.error) {
+    return NextResponse.json({ error: res.error }, { status: 400 });
   }
   return NextResponse.json({ status: 200 });
 }
